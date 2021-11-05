63c9c6dc49887f8a4bb258ba14d649e05f3520be
yashinatu <yashinatu2007@gmail.com>
2021-11-05 16:00:40 +0530

    --

--- app.js
+++ app.js
@@ -25,9 +25,10 @@ app.use(session({
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
 const postRoute = require('./routes/postRoutes');
-const profileRoute = require('./routes/profileRoutes');
 const logoutRoute = require('./routes/logout');
+const profileRoute = require('./routes/profileRoutes');
 const uploadRoute = require('./routes/uploadRoutes');
+const searchRoute = require('./routes/searchRoutes');
 // Api routes
 const postsApiRoute = require('./routes/api/posts');
 const usersApiRoute = require('./routes/api/users');
@@ -38,6 +39,7 @@ app.use("/logout", logoutRoute);
 app.use("/posts", middleware.requireLogin, postRoute);
 app.use("/profile", middleware.requireLogin, profileRoute);
 app.use("/uploads", uploadRoute);
+app.use("/search", middleware.requireLogin, searchRoute);
 
 app.use("/api/posts", postsApiRoute);
 app.use("/api/users", usersApiRoute);
--- routes/api/users.js
+++ routes/api/users.js
@@ -11,6 +11,27 @@ const Post = require('../../schemas/PostSchema');
 
 app.use(express.urlencoded({ extended: false }));
 
+router.get("/", async (req, res, next) => {
+    var searchObj = req.query;
+
+    if(req.query.search !== undefined) {
+        searchObj = {
+            $or: [
+                { firstName: { $regex: req.query.search, $options: "i" }},
+                { lastName: { $regex: req.query.search, $options: "i" }},
+                { username: { $regex: req.query.search, $options: "i" }},
+            ]
+        }
+    }
+
+    User.find(searchObj)
+    .then(results => res.status(200).send(results))
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+});
+
 router.put("/:userId/follow", async (req, res, next) => {
 
     var userId = req.params.userId;
@@ -66,20 +87,40 @@ router.post("/profilePicture", upload.single("croppedImage"), async (req, res, n
         console.log("No file uploaded with ajax request.");
         return res.sendStatus(400);
     }
-    //console.log(req.file.filename);
-    var filePath = `/uploads/images/${req.file.filename}.jpeg`;
+
+    var filePath = `/uploads/images/${req.file.filename}.png`;
     var tempPath = req.file.path;
     var targetPath = path.join(__dirname, `../../${filePath}`);
-    console.log(tempPath);
-    console.log(targetPath);
+
     fs.rename(tempPath, targetPath, async error => {
         if(error != null) {
             console.log(error);
-            console.log("Yashi");
             return res.sendStatus(400);
         }
 
         req.session.user = await User.findByIdAndUpdate(req.session.user._id, { profilePic: filePath }, { new: true });
+        res.sendStatus(204);
+    })
+
+});
+
+router.post("/coverPhoto", upload.single("croppedImage"), async (req, res, next) => {
+    if(!req.file) {
+        console.log("No file uploaded with ajax request.");
+        return res.sendStatus(400);
+    }
+
+    var filePath = `/uploads/images/${req.file.filename}.png`;
+    var tempPath = req.file.path;
+    var targetPath = path.join(__dirname, `../../${filePath}`);
+
+    fs.rename(tempPath, targetPath, async error => {
+        if(error != null) {
+            console.log(error);
+            return res.sendStatus(400);
+        }
+
+        req.session.user = await User.findByIdAndUpdate(req.session.user._id, { coverPhoto: filePath }, { new: true });
         res.sendStatus(204);
     })
 
