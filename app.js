a904974a3049efba0703abfc6d8a57fbe610fa1a
yashinatu <yashinatu2007@gmail.com>
2021-11-08 18:38:51 +0530

    -

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require("body-parser")
